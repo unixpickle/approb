414e0cdd0ff1bf93a83e6757ec0a8c0422ba2323
Alex Nichol <unixpickle@gmail.com>
2016-10-04 13:32:45 -0400

    implemented urn drawing API

---
+++ README.md
@@ -0,0 +1,3 @@
+# probability
+
+This package will contain a set of tools for approximating various probabilities. It is mainly intended to be used to check one's manual calculations.
---
+++ urn.go
@@ -0,0 +1,46 @@
+package main
+
+import "math/rand"
+
+// An Urn contains an immutable set of integers.
+type Urn []int
+
+// NewUrn creates an urn with integers 0 through
+// len(s)-1, where there are s[i] instances of i.
+func NewUrn(s []int) Urn {
+	var res Urn
+	for i, c := range s {
+		for j := 0; j < c; j++ {
+			res = append(res, i)
+		}
+	}
+	return res
+}
+
+// Draw chooses a random element from the urn.
+// It returns a new urn with the element removed.
+// The urn must not be empty.
+func (u Urn) Draw() (int, Urn) {
+	if len(u) == 0 {
+		panic("urn is empty")
+	}
+	idx := rand.Intn(len(u))
+	val := u[idx]
+	newUrn := make(Urn, len(u)-1)
+	copy(newUrn, u[:idx])
+	copy(newUrn[idx:], u[idx+1:])
+	return val, newUrn
+}
+
+// Choose randomly selects n elements from the urn
+// without replacement.
+// The urn must contain at least n elements.
+func (u Urn) Choose(n int) (choice, remainder Urn) {
+	remainder = u
+	for i := 0; i < n; i++ {
+		var el int
+		el, remainder = remainder.Draw()
+		choice = append(choice, el)
+	}
+	return
+}
