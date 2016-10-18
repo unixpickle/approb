83fb18bead09c9acf5e75505b90c8215d1b7abb0
Alex Nichol <unixpickle@gmail.com>
2016-10-18 13:02:12 -0400

    implemented indicator utility

--- moments.go
+++ moments.go
@@ -16,3 +16,15 @@ func Moments(n int, f func() float64) (mean, variance float64) {
 	variance = squareSum.Sum()/float64(n) - mean*mean
 	return
 }
+
+// Indicator is like Moments, but f returns a bool
+// which is treated like a 1 if true and 0 if false.
+func Indicator(n int, f func() bool) (mean, variance float64) {
+	return Moments(n, func() float64 {
+		if f() {
+			return 1
+		} else {
+			return 0
+		}
+	})
+}
